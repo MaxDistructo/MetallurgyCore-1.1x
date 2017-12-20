8d8e8dbbea11dd15b0fac524efc983a955abd9ea
MaxDistructo <MaxDistructo@users.noreply.github.com>
2017-12-20 12:05:00 -0600

    Update ItemOreFinder.java

--- src/main/java/com/teammetallurgy/metallurgycore/utils/ItemOreFinder.java
+++ src/main/java/com/teammetallurgy/metallurgycore/utils/ItemOreFinder.java
@@ -23,9 +23,9 @@ import net.minecraftforge.oredict.OreDictionary;
 
 import com.teammetallurgy.metallurgycore.MetallurgyCore;
 
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemOreFinder extends Item
 {
